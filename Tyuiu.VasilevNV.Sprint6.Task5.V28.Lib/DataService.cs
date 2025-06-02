7a4949cf7fa1515049e605753e55bac3c51dc918
Николай Васильев <nvasiljev1977@gmail.com>
2025-06-02 18:58:03 +0500

    Task5

--- Tyuiu.VasilevNV.Sprint6.Task5.V28.Lib/DataService.cs
+++ Tyuiu.VasilevNV.Sprint6.Task5.V28.Lib/DataService.cs
@@ -1,44 +1,28 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
 namespace Tyuiu.VasilevNV.Sprint6.Task5.V28.Lib
 {
     public class DataService : ISprint6Task5V28
     {
         public double[] LoadFromDataFile(string path)
         {
-            if (!File.Exists(path))
-                throw new FileNotFoundException("Файл не найден", path);
-
-            List<double> values = new List<double>();
-
-            // Читаем строки из файла
+            // Читаем все строки из файла
             string[] lines = File.ReadAllLines(path);
 
-            foreach (string line in lines)
-            {
-                // Разбиваем строку на элементы, предполагая, что числа разделены пробелами
-                string[] elements = line.Split(new[] { ' ', '\t', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            // Преобразуем строки в числа, фильтруем их и округляем
+            double[] numsArray = lines
+                .Select(line => Convert.ToDouble(line))
+                .Select(val => Math.Round(val, 3)) // Округляем до 3 знаков после запятой
+                .Where(val => val < 10) // Оставляем только числа меньше 10
+                .ToArray();
 
-                foreach (string element in elements)
-                {
-                    if (double.TryParse(element, out double number))
-                    {
-                        // Округляем до трех знаков после запятой и добавляем в список
-                        values.Add(Math.Round(number, 3));
-                    }
-                }
+            // Выводим результат
+            foreach (var num in numsArray)
+            {
+                Console.WriteLine(num);
             }
 
-            return values.ToArray();
-        }
-
-        /// <summary>
-        /// Метод фильтрует числа, меньшие 10, из массива.
-        /// </summary>
-        /// <param name="data">Массив чисел.</param>
-        /// <returns>Массив чисел, меньших 10.</returns>
-        public double[] GetNumbersLessThanTen(double[] data)
-        {
-            return data.Where(x => x < 10).ToArray();
+            return numsArray;
         }
     }
 }
